800257f05cfa13e57e9a125f24d140fe47cffb25
CARGOMATRIX\ashot.nazaryan <ashot.nazaryan@cargomatrix.com>
2017-06-16 15:33:50 +0400

    commit -w

--- server.js
+++ server.js
@@ -6,11 +6,7 @@ const firebase = require('firebase');
 const admin = require("firebase-admin");
 const serviceAccount = require("./firebase-service-account.json");
 
-<<<<<<< HEAD
 const port = 1234;
-=======
-const port = 5000;
->>>>>>> 35245ef391c1b4ecd18b591e6c1d53134fb4d9bb
 
 admin.initializeApp({
     databaseURL: 'https://gapp-def88.firebaseio.com',
