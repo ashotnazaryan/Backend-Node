20a857b5c9dda64b68cd95c15232d4b27520e4b5
Ashot <ashotnazaryan45@outlook.com>
2017-06-17 21:48:36 +0400

    commit -h

--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -1,9 +1,9 @@
 module.exports = (app, db) => {
 
-  const ref = db.ref('/datasets');
+  const ref = db.ref('/data');
   const cors = require('cors');
   
-  app.post('/notes', cors(), (req, res) => {
+  app.post('/api/notes', cors(), (req, res) => {
     ref.once("value")
       .then((snap) => {
         res.send(snap.val());
